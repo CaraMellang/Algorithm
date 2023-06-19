4634b82dc70f574814d855641f382e382a81bd89
Mellang <51808985+CaraMellang@users.noreply.github.com>
2023-06-19 14:40:27 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 102.02 ms, Memory: 57 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -0,0 +1,17 @@
+function solution(participant, completion) {
+    let  answer;
+    
+    participant.sort();
+    completion.sort()
+    
+    for(let i = 0 ; i < participant.length; i++){
+        if(participant[i] !== completion[i]){
+            answer = participant[i];
+            break;
+        }
+    }
+    
+    
+    return answer;
+    // return participant.filter((element) => !completion.includes(element))[0];
+}
